50d991752a62e78f72c4021b25b82834a18cbd47
3dots <3dot@live.ca>
2023-12-13 13:08:57 -0500

    Play ground 1

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max));
+}
+
+function Header() {
+
+  const randText = () => {
+    return reactDescriptions[genRandomInt(reactDescriptions.length)];
+  };
+
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {randText()} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
