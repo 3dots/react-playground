e3f4b8ca58bdf5f722120d4ac86e603ce0557beb
3dots <3dot@live.ca>
2024-01-30 12:15:16 -0500

    Create template for future use

---
+++ template/src/index.tsx
@@ -0,0 +1,18 @@
+import "./index.scss"
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { App } from "./App.tsx"
+import { Provider } from "react-redux"
+import { store } from "./app/store.ts"
+import { IntlProvider } from "react-intl"
+import messages_en from "./translations/en.json"
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <IntlProvider locale="en-CA" messages={messages_en}>
+        <App />
+      </IntlProvider>
+    </Provider>
+  </React.StrictMode>,
+)
