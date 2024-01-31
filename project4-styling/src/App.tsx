f299dd2290063129d1905d712b1cae0aedc3edf1
3dots <3dot@live.ca>
2024-01-31 08:17:30 -0500

    Move CSS

--- project4-styling/src/App.tsx
+++ project4-styling/src/App.tsx
@@ -1,10 +1,11 @@
 import AuthInputs from "./components/AuthInputs/AuthInputs"
 import { HeaderContent } from "./components/Header/Header"
+import cssClasses from "./App.module.scss"
 
 export function App() {
   return (
     <>
-      <header className="header">
+      <header className={cssClasses.header}>
         <HeaderContent />
       </header>
       <main className="main">
