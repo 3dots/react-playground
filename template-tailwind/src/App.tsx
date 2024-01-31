e266ee83710d64d80004997f0cf0c7c8cb74af96
3dots <3dot@live.ca>
2024-01-31 09:12:22 -0500

    Play around with Tailwind. I like it.

---
+++ template-tailwind/src/App.tsx
@@ -0,0 +1,18 @@
+import { HeaderContent } from "./components/Header/Header"
+import { FormattedMessage } from "./components/Intl/Intl"
+
+export function App() {
+  return (
+    <>
+      <header className="header">
+        <HeaderContent />
+      </header>
+      <main className="main">
+        <h1 className="center">
+          <FormattedMessage id="app.title" />
+        </h1>
+        Hello world!
+      </main>
+    </>
+  )
+}
